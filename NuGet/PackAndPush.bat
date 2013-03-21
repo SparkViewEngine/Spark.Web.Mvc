30367ed79de328ec9b1ca19d4ac668fef1623196
Robert Greyling <robertgreyling@gmail.com>
2013-03-21 22:47:31 +0000

    Remove Nuget creation - belongs only on master

--- NuGet/PackAndPush.bat
+++
@@ -1,9 +0,0 @@
-rem nuget pack Spark.Web.Mvc2\Spark.Web.Mvc2.nuspec
-rem nuget pack Spark.Web.Mvc3\Spark.Web.Mvc3.nuspec
-nuget pack Spark.Web.Mvc4\Spark.Web.Mvc4.nuspec
-
-rem nuget push Spark.Web.Mvc2.1.7.5.1.nupkg
-rem nuget push Spark.Web.Mvc3.1.7.5.1.nupkg
-nuget push Spark.Web.Mvc4.1.0.0.1.nupkg
-
-pause
